d78554309c05583f4a1d86e60e95151dbf85d24c
Md Majharul Islam <80042446+majharul-web@users.noreply.github.com>
2021-09-25 21:32:15 +0600

    add button icon

--- src/components/Food/Food.js
+++ src/components/Food/Food.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee, faShoppingBasket } from '@fortawesome/free-solid-svg-icons';
 
 const Food = (props) => {
     const { strMeal, strMealThumb, strInstructions } = props.food;
     // console.log(props)
+    const element = <FontAwesomeIcon icon={faShoppingBasket} />
     return (
 
         <div className="card" style={{ maxWidth: "540px" }}>
@@ -14,7 +17,7 @@ const Food = (props) => {
                     <div className="card-body">
                         <h5 className="card-title">Name: {strMeal}</h5>
                         <p className="card-text"><span className="fw-bolder">Details: </span>{strInstructions.slice(0, 100)}</p>
-                        <button onClick={() => props.handleAddProduct(strMeal)} className="btn btn-danger">Add To Cart</button>
+                        <button onClick={() => props.handleAddProduct(strMeal)} className="btn btn-danger">{element} Add To Cart</button>
                     </div>
                 </div>
             </div>
